c29df1a967f45877c4fc4f2c961f631c7152cddb
minatony <73822935+minatony@users.noreply.github.com>
2022-05-15 11:09:13 +0700

    Update Simple_tic_tac_toe.py
    fix

--- projects/Simple_tic_tac_toe.py
+++ projects/Simple_tic_tac_toe.py
@@ -99,8 +99,8 @@ win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 
 turn = 0
 finish = True
 while finish:
+    re_inp = True
     while re_inp:
-        re_inp = True
         check_num = 0
         check_inp = 0
         inp = input("Enter the coordinates: ").split()
