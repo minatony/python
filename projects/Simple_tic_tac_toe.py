b5e2c12d2e2599b1e5c224f7d9ad8766c5e4cc36
minatony <73822935+minatony@users.noreply.github.com>
2022-05-15 11:07:42 +0700

    Update Simple_tic_tac_toe.py
    add finish part

--- projects/Simple_tic_tac_toe.py
+++ projects/Simple_tic_tac_toe.py
@@ -82,3 +82,86 @@ print('|', enter_cells[0], enter_cells[1], enter_cells[2], '|')
 print('|', enter_cells[3], enter_cells[4], enter_cells[5], '|')
 print('|', enter_cells[6], enter_cells[7], enter_cells[8], '|')
 print("---------")
+"""
+/////////////////////////////////////////////////////finish
+"""
+cells = list('         ')
+print(
+    "---------\n"
+    "|", cells[0], cells[1], cells[2], "|\n"
+    "|", cells[3], cells[4], cells[5], "|\n"
+    "|", cells[6], cells[7], cells[8], "|\n"
+    "---------"
+)
+number = "1 2 3 4 5 6 7 8 9 0".split()
+new_cellS = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]]
+win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+turn = 0
+finish = True
+while finish:
+    while re_inp:
+        re_inp = True
+        check_num = 0
+        check_inp = 0
+        inp = input("Enter the coordinates: ").split()
+        for i in range(2):
+            if inp[i] in number:
+                check_num += 1
+                if 0 < int(inp[i]) < 4:
+                    check_inp += 1
+        if check_num < 2:
+            print("You should enter numbers!")
+            continue
+        else:
+            if check_inp < 2:
+                print("Coordinates should be from 1 to 3!")
+                continue
+            else:
+                for i in range(9):
+                    if int(inp[0]) == new_cellS[i][0] and int(inp[1]) == new_cellS[i][1]:
+                        if cells[i] == 'X' or cells[i] == 'O':
+                            print("This cell is occupied! Choose another one!")
+                            break
+                        else:
+                            if turn % 2 == 0:
+                                cells[i] = 'X'
+                            else:
+                                cells[i] = 'O'
+                            turn += 1
+                            re_inp = False
+    print(
+        "---------\n"
+        "|", cells[0], cells[1], cells[2], "|\n"
+        "|", cells[3], cells[4], cells[5], "|\n"
+        "|", cells[6], cells[7], cells[8], "|\n"
+        "---------"
+    )
+    play_one = 0
+    play_two = 0
+    draw = 0
+    game_finish = 0
+    for i in range(8):
+        a = win[i][0]
+        b = win[i][1]
+        c = win[i][2]
+        if cells[a] == cells[b] == cells[c] == 'X':
+            play_one += 1
+        elif cells[a] == cells[b] == cells[c] == 'O':
+            play_two += 1
+    if play_one != 0 and play_two == 0:
+        print("X wins")
+        finish = False
+    elif play_one == 0 and play_two != 0:
+        print("O wins")
+        finish = False
+    elif play_one == 0 and play_two == 0:
+        for i in range(9):
+            if cells[i] == 'X' or cells[i] == 'O':
+                draw += 1
+            else:
+                game_finish += 1
+        if draw == 9:
+            print("Draw")
+            finish = False
+        if game_finish != 0:
+            finish = True
